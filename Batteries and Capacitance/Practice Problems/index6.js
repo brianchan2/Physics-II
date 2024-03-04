8335b4f33777073f7b977d99461b97d916dc8c2a
Brian Chan <brianchan9061@gmail.com>
2024-03-04 05:25:46 -0500

    Added questions

---
+++ Batteries and Capacitance/Practice Problems/index6.js
@@ -0,0 +1,16 @@
+let e0 = 8.85 * 10 ** -12
+let a = 0.02 ** 2
+let d = 0.001
+let c = e0 * a /d
+let v = 9
+let q = c * v
+
+console.log(q + " C", v + "V")
+
+// b.
+
+d = 0.002
+c = e0 * a /d
+q = c * v
+
+console.log(q + " C", v + "V")
