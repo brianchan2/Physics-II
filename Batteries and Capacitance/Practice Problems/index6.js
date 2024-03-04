ee620317e3a88b1bb9af6334050e39d927d9baef
Brian Chan <brianchan9061@gmail.com>
2024-03-04 05:26:14 -0500

    Added questions

--- Batteries and Capacitance/Practice Problems/index6.js
+++ Batteries and Capacitance/Practice Problems/index6.js
@@ -1,3 +1,15 @@
+/**
+ *  Knight Problem 26.53) Two 2.0 cm × 2.0 cm metal elec-
+    trodes are spaced 1.0 mm apart and connected by wires to
+    the terminals of a 9.0 V battery.
+    a. What are the charge on each electrode and the potential
+    difference between them? [Answer: ±3.2 × 10−11 C, 9.0 V.]
+    The wires are disconnected, and insulated handles are used
+    to pull the plates apart to a new spacing of 2.0 mm.
+    b. What are the charge on each electrode and the potential
+    difference between them? [Answer: ±3.2 × 10−11 C, 18 V.
+ */
+
 let e0 = 8.85 * 10 ** -12
 let a = 0.02 ** 2
 let d = 0.001
